6790238c96e4379620e814600b673d7c92d64646
Dave Walker <dave@happybits.co>
2018-10-14 14:54:47 -0700

    implement login and authenticated requests without hbmx

--- app/src/main/java/co/happybits/mpcompanion/LoginManager.java
+++ app/src/main/java/co/happybits/mpcompanion/LoginManager.java
@@ -1,44 +1,198 @@
-//package co.happybits.mpcompanion;
-//
-//import co.happybits.hbmx.mp.ApplicationIntf;
-//import co.happybits.hbmx.mp.RegistrationSendType;
-//import co.happybits.hbmx.mp.StartRegistrationResponse;
-//import co.happybits.hbmx.mp.StartRegistrationStatus;
-//import co.happybits.hbmx.mp.UserManagerIntf;
-//import co.happybits.hbmx.mp.VerifyRegistrationResponse;
-//import co.happybits.hbmx.mp.VerifyRegistrationStatus;
-//import co.happybits.hbmx.tasks.Task;
-//import co.happybits.hbmx.tasks.TaskObservable;
-//
-//public class LoginManager {
-//
-//    static TaskObservable<Boolean> login(final String phone, final String countryCode) {
-//
-//        // phone: the local phone number including area code (e.g. 2065551234)
-//        // countryCode: the two letter country identifier (e.g. "US")
-//
-//        return new Task<Boolean>() {
-//
-//            @Override
-//            public Boolean access() {
-//
-//                UserManagerIntf userManager = ApplicationIntf.getUserManager();
-//                if (userManager.isRegistered()) {
-//                    return true;
-//                }
-//
-//                StartRegistrationResponse startResponse = userManager.startRegistrationForPhone(phone, countryCode, RegistrationSendType.SMS);
-//                if (startResponse.getStatus() != StartRegistrationStatus.NO_ERROR) {
-//                    return false;
-//                }
-//
-//                VerifyRegistrationResponse verifyResponse = userManager.autoVerify();
-//                if (verifyResponse.getStatus() != VerifyRegistrationStatus.NO_ERROR) {
-//                    return false;
-//                }
-//
-//                return userManager.isRegistered();
-//            }
-//        }.submit();
-//    }
-//}
+package co.happybits.mpcompanion;
+
+import android.content.Context;
+import android.content.SharedPreferences;
+import android.os.Build;
+import android.util.Base64;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.IOException;
+import java.nio.ByteBuffer;
+import java.util.HashMap;
+import java.util.UUID;
+
+import co.happybits.mpcompanion.networking.PlatformHttpConnection;
+import okhttp3.Response;
+
+public class LoginManager {
+
+    private static String API_BASE_URL = "https://dev.marcopolo.me/api/v4";
+
+    private static LoginManager _instance;
+
+    private final String _deviceID;
+    private final String _secret;
+
+    private boolean _isRegistered;
+
+    private String _apiToken;
+
+    static synchronized LoginManager getInstance() {
+        if (_instance == null) {
+            _instance = new LoginManager();
+        }
+
+        return _instance;
+    }
+
+    private LoginManager() {
+        Context context = MpCompanion.instance.getApplicationContext();
+        SharedPreferences prefs = context.getSharedPreferences(context.getPackageName(), Context.MODE_PRIVATE);
+        _deviceID = prefs.getString("DeviceID", createXID());
+        _secret = prefs.getString("AuthSecret", createXID());
+        _isRegistered = prefs.getBoolean("IsRegistered", false);
+    }
+
+    boolean login(final String phone, final String countryCode) {
+
+        // phone: the local phone number including area code (e.g. 2065551234)
+        // countryCode: the two letter country identifier (e.g. "US")
+
+        if (_isRegistered) {
+            return true;
+        }
+
+        try {
+            sendPhoneCode(phone, countryCode);
+
+            String code = getAutoVerifyPhoneCode(phone, countryCode);
+
+            verifyCode(phone, countryCode, code);
+
+        } catch (IOException ex) {
+            // network problem
+        }
+
+        return _isRegistered;
+    }
+
+    public synchronized String getApiToken() {
+        return _apiToken;
+    }
+
+    public synchronized boolean isRegistered() {
+        return _isRegistered;
+    }
+
+    public void auth() throws IOException {
+
+        try {
+            JSONObject authParams = new JSONObject()
+                .put("device_id", _deviceID)
+                .put("secret", _secret)
+                .put("app_type", "mp")
+                .put("app_version", "1.0.0")
+                .put("app_build", 1000)
+                .put("platform_type", "android")
+                .put("platform_version", 24)
+                .put("manufacturer", Build.MANUFACTURER)
+                .put("model_name", Build.MODEL)
+                .put("flavor", "dev")
+                .put("locale", "en_US")
+                .put("timezone", "PST");
+
+            PlatformHttpConnection connection = new PlatformHttpConnection(API_BASE_URL + "/auth");
+            Response response = connection.executeRequest(PlatformHttpConnection.POST, new HashMap<>(), authParams);
+            JSONObject responseJSON = new JSONObject(new String(response.body().bytes()));
+
+            parseAuthResponse(responseJSON);
+
+        } catch (JSONException ex) {
+            // empty
+        }
+    }
+
+    private void sendPhoneCode(final String phone, final String countryCode) throws IOException {
+
+        try {
+            JSONObject params = new JSONObject()
+                .put("phone", phone)
+                .put("country_code", countryCode);
+
+            doAuthenticatedRequest(PlatformHttpConnection.POST, "/auth/send-phone-code", params);
+
+        } catch (JSONException ex) {
+            // empty
+        }
+    }
+
+    private String getAutoVerifyPhoneCode(final String phone, final String countryCode) throws IOException {
+
+        try {
+            JSONObject params = new JSONObject()
+                .put("phone", phone)
+                .put("country_code", countryCode);
+
+            JSONObject responseJSON = doAuthenticatedRequest(PlatformHttpConnection.POST, "/admin/user/phone_code", params);
+            return responseJSON.getString("code");
+
+        } catch (JSONException ex) {
+            // empty
+        }
+
+        return null;
+    }
+
+    private boolean verifyCode(final String phone, final String countryCode, final String code) throws IOException {
+
+        try {
+            JSONObject params = new JSONObject()
+                .put("phone", phone)
+                .put("country_code", countryCode)
+                .put("verification_code", code);
+
+            JSONObject responseJSON = doAuthenticatedRequest(PlatformHttpConnection.POST, "/auth/verify-phone-code", params);
+            parseAuthResponse(responseJSON);
+
+        } catch (JSONException ex) {
+            // empty
+        }
+
+        return _isRegistered;
+    }
+
+    private JSONObject doAuthenticatedRequest(String method, String path, JSONObject params) throws IOException {
+
+        try {
+            PlatformHttpConnection connection = new PlatformHttpConnection(API_BASE_URL + path);
+            HashMap<String, String> headers = new HashMap<>();
+            headers.put("Authorization", "Bearer " + _apiToken);
+
+            Response response = connection.executeRequest(method, headers, params);
+            if (response.code() == 401) {
+                auth();
+                headers.put("Authorization", "Bearer " + _apiToken);
+                response = connection.executeRequest(method, headers, params);
+            }
+            return new JSONObject(new String(response.body().bytes()));
+
+        } catch (JSONException ex) {
+            return new JSONObject();
+        }
+    }
+
+    private synchronized void parseAuthResponse(JSONObject responseJSON) {
+        try {
+            _apiToken = responseJSON.getString("api_token");
+            _isRegistered = responseJSON.getBoolean("is_registered");
+        } catch (JSONException ex) {
+            // empty
+        }
+    }
+
+    /**
+     * Creates a new XID based on a random UUID (uuid4).
+     */
+    public static String createXID() {
+        UUID uuid = UUID.randomUUID();
+
+        ByteBuffer buffer = ByteBuffer.wrap(new byte[16]);
+        buffer.putLong(uuid.getMostSignificantBits());
+        buffer.putLong(uuid.getLeastSignificantBits());
+
+        // Return only the first 22 characters.
+        return Base64.encodeToString(buffer.array(), Base64.URL_SAFE).substring(0, 22);
+    }
+}
---
+++ app/src/main/java/co/happybits/mpcompanion/networking/PlatformHttpSession.java
@@ -0,0 +1,40 @@
+package co.happybits.mpcompanion.networking;
+
+import java.util.concurrent.TimeUnit;
+import okhttp3.ConnectionPool;
+import okhttp3.OkHttpClient;
+
+public class PlatformHttpSession {
+
+    private static final int IDLE_CONNECTION_COUNT = 5;
+    private static final int IDLE_CONNECTION_TIMEOUT_SECONDS = 60;
+
+    private static PlatformHttpSession _sharedSession;
+    private OkHttpClient _client;
+
+    public static synchronized PlatformHttpSession sharedSession() {
+        if (_sharedSession == null) {
+            _sharedSession = new PlatformHttpSession();
+        }
+        return _sharedSession;
+    }
+
+    public PlatformHttpSession() {
+        ConnectionPool pool = new ConnectionPool(IDLE_CONNECTION_COUNT, IDLE_CONNECTION_TIMEOUT_SECONDS, TimeUnit.SECONDS);
+
+        OkHttpClient.Builder builder = new OkHttpClient.Builder()
+            .connectionPool(pool)
+            .readTimeout(30, TimeUnit.SECONDS)
+            .writeTimeout(30, TimeUnit.SECONDS);
+
+        // Add a 10 second ping interval to help alleviate SocketTimeoutException
+        // https://github.com/square/okhttp/issues/3146
+        builder.pingInterval(10, TimeUnit.SECONDS);
+
+        _client = builder.build();
+    }
+
+    public OkHttpClient getClient() {
+        return _client;
+    }
+}
