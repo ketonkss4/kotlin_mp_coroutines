0972f285a3913eb31af22910c7e9288ea0c8a29f
Kevin <kevin@powermovesdev.com>
2018-10-16 01:56:06 -0700

    update data request test

--- app/src/test/java/co/happybits/mpcompanion/util/DataHelper.kt
+++ app/src/test/java/co/happybits/mpcompanion/util/DataHelper.kt
@@ -1,17 +1,18 @@
 package co.happybits.mpcompanion.util
 
 import co.happybits.mpcompanion.data.Conversation
+import co.happybits.mpcompanion.data.Messages
 
 fun buildTestConversation(
         group: Boolean = false,
         title: String = "",
-        conversation_id: Int = 0
+        conversation_id: String = "0"
 ): Conversation {
     return Conversation(group,
             title,
+            "",
             conversation_id,
             "",
-            emptyList(),
-            emptyList(),
+            Messages(emptyList()),
             emptyList())
 }
--- app/src/test/java/co/happybits/mpcompanion/widget/WidgetViewModelTest.kt
+++ app/src/test/java/co/happybits/mpcompanion/widget/WidgetViewModelTest.kt
@@ -3,6 +3,8 @@ package co.happybits.mpcompanion.widget
 import androidx.lifecycle.MutableLiveData
 import co.happybits.mpcompanion.concurrency.AppDispatchers
 import co.happybits.mpcompanion.concurrency.KtDispatchers
+import co.happybits.mpcompanion.data.Conversation
+import co.happybits.mpcompanion.data.Response
 import co.happybits.mpcompanion.networking.ServiceClientHelper
 import co.happybits.mpcompanion.util.buildTestConversation
 import kotlinx.coroutines.experimental.GlobalScope
@@ -10,38 +12,41 @@ import kotlinx.coroutines.experimental.async
 import kotlinx.coroutines.experimental.runBlocking
 import org.junit.Rule
 import org.junit.Test
-import org.mockito.ArgumentCaptor
-import org.mockito.InjectMocks
-import org.mockito.Mock
-import org.mockito.Mockito
+import org.mockito.*
 import org.mockito.Mockito.times
 import org.mockito.Mockito.verify
 import org.mockito.junit.MockitoJUnit
 
 
 class WidgetViewModelTest {
-    @get:Rule val mockitoRule = MockitoJUnit.rule()
-    @Mock lateinit var poloService: ServiceClientHelper.PoloService
-    @Mock lateinit var dispatchers: KtDispatchers
-    @Mock lateinit var poloWidgetData: MutableLiveData<PoloWidget>
-    @InjectMocks lateinit var widgetViewModel: WidgetViewModel
+    @get:Rule
+    val mockitoRule = MockitoJUnit.rule()
+    @Mock
+    lateinit var poloService: ServiceClientHelper.PoloService
+    @Mock
+    lateinit var dispatchers: KtDispatchers
+    @Mock
+    lateinit var poloWidgetData: MutableLiveData<List<Conversation>>
+    @InjectMocks
+    lateinit var widgetViewModel: WidgetViewModel
+    @Captor
+    private lateinit var captor: ArgumentCaptor<List<Conversation>>
 
     @Test
-    fun testConversationDataRequest(){
-        val testConversation = buildTestConversation(conversation_id = 100)
+    fun testConversationDataRequest() {
+        val testConversation = buildTestConversation(conversation_id = "100")
         val testList = arrayListOf(testConversation)
-        val deferredMock = GlobalScope.async { testList }
+        val response = Response("", "", testList)
+        val deferredMock = GlobalScope.async { response }
 
         Mockito.`when`(dispatchers.ioDispatcher()).thenReturn(AppDispatchers().testDispatcher())
 
         Mockito.`when`(runBlocking { poloService.requestConversationSync() }).thenReturn(deferredMock)
 
-        widgetViewModel.syncWidgetData()
-
-        val argumentCaptor = ArgumentCaptor.forClass(PoloWidget::class.java)
-        verify(poloWidgetData, times(1)).postValue(argumentCaptor.capture())
-        val poloWidget = argumentCaptor.value
-        assert(poloWidget.conversationId == 100)
+        widgetViewModel.requestConversationsListData()
+        verify(poloWidgetData, times(1)).postValue(captor.capture())
+        val conversation = captor.value.first()
+        assert(conversation.conversation_id == "100")
     }
 
 
